400a418973b70f509894de75ac3478cd620538b7
Rownokzahan <84782117+Rownokzahan@users.noreply.github.com>
2023-02-23 14:17:52 +0600

    initial commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const taskList = document.querySelector('ul');
+const addForm = document.getElementById('add-form');
+const updateForm = document.getElementById('update-form');
+
+taskList.addEventListener('click', function (e) {
+    const taskContainer = e.target.closest('li div.flex');
+    if (!taskContainer) {
+        return;
+    }
+    taskContainer.classList.toggle('line-through');
+
+    const icon = taskContainer.querySelector('div .bi');
+    icon.classList.toggle('bi-circle');
+    icon.classList.toggle('bi-check-circle-fill');
+
+    const buttons = taskContainer.parentNode.querySelectorAll('button');
+    for (const button of buttons) {
+        button.classList.toggle('hidden');
+    }
+});
+
+document.getElementById('new-task-btn').addEventListener('click', function () {
+    addForm.classList.toggle('hidden');
+});
